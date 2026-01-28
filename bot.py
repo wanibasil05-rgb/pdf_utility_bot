ba20a81789f406b4fa3f66b5e8913beec26383be
wanibasil05-rgb <wanibasil05@gmail.com>
2026-01-28 18:57:11 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import asyncio
 import os
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message, ContentType
-from config import BOT_TOKEN
+from aiogram.types import Message
 from PIL import Image
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
@@ -19,7 +20,7 @@ async def start(message: Message):
         "Send me a PHOTO and I will convert it to PDF."
     )
 
-@dp.message(lambda message: message.content_type == ContentType.PHOTO)
+@dp.message(F.photo)
 async def image_to_pdf(message: Message):
     photo = message.photo[-1]
     file = await bot.get_file(photo.file_id)
